a37e23654b6dff1353f88ae4f70f2cd94153a51e
Zsolt Vadon <2021307@dszcberegszaszi.hu>
2022-02-03 10:04:10 +0100

    orai-muka

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Hello
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Konstansok
+            const string fejleszto= "Vadon Zsolt";
+            
+            
+            //Változok
+            string uzenet = "Hello World!";
+            int szam = 10;
+            double szam2 = 10.3;
+            bool gender = true;
+            char karakter = 's';
+            string szöveg = "szöveg";
+            
+            //program törzse  
+            Console.WriteLine(uzenet);
+            Console.WriteLine(szam);
+            Console.WriteLine(szam2);
+            Console.WriteLine(gender);
+            Console.WriteLine(karakter);
+            Console.WriteLine(szöveg);
+            Console.ReadKey();
         }
     }
 }
