e24c6261d52e791ebcf3f0d07e09ac6b34f170e5
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-02-21 14:26:01 +0100

    Átlaghőmérséklet

---
+++ atlaghomerseklet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlaghomerseklet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tomb = new int[12, 30];
+            int a, b;
+            Random r_random = new Random();
+
+            for (int i = 0; i < tomb.GetLength(0); i++)
+            {
+                for(int j = 0; i < tomb.GetLength(i) j++)
+                {
+                    tomb[0, i] = r_random.Next(-12, 15);
+                    
+                }
+
+
+            }
+
+        }
+    }
+}
