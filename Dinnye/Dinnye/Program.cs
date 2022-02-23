e57951b6b45c25be28aafae4557d76417a79f9bc
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-02-23 11:41:18 +0100

    Dinnye
    Dinnye feladat 2022.02.23

---
+++ Dinnye/Dinnye/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dinnye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int dinnye =int.Parse(Console.ReadLine());
+          int n =int.Parse(Console.ReadLine();
+          
+          Console.WriteLine("Add meg a dinnye átmérőjét: ");
+          Console.WriteLine();
+          
+          int n =int.Parse(Console.ReadLine();
+          Console.WriteLine("Add meg a dinnyék számát: ");
+
+          double szalag (2 * dinnye * Math.PI) + 60) * n;
+          Console.WriteLine();
+          
+          Console.WriteLine("A szükséges szalag () cm." szalag );
+          Console.ReadLine();
+
+
+          
+
+        }
+    }
+}
