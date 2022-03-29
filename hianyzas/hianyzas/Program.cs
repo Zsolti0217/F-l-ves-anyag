48fc97b7dd01e8202d3a633fe85ae996aadda16c
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-03-29 11:29:58 +0200

    Hiányzás

---
+++ hianyzas/hianyzas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace hianyzas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hány hiányzás van?");
+            int hianyzas = Convert.ToInt32(Console.ReadLine));
+
+            if (hianyzas > 9)
+            {
+                Console.WriteLine("Add meg a születési dátumod: ");
+                string szuleadat = Console.ReadLine();
+
+                Console.WriteLine("Ekkor születet: {0}", szuleadat, "és ennyit hiányzot: {0}", hianyzas);
+            }
+
+
+            if ((9 < hianyzas) && (hianyzas < 16)) 
+            {
+                Console.WriteLine("Figyelmeztetés: ");
+
+            }
+            else if ((hianyzas > 15) && (hianyzas < 20))
+            {
+                Console.WriteLine("Osztályfönőki megrovás");
+            }
+            else if ((hianyzas > 18) && (hianyzas < 30))
+            {
+                Console.WriteLine("Igazgatói megrovás");
+            }
+
+            else
+            {
+                Console.WriteLine("megrovás");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
