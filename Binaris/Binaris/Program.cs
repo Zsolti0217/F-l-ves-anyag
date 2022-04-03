6ad9dd1e4b33abf6ce788a2dff3a5a19a3f6a9cb
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-04-03 21:24:01 +0200

    Add files via upload

---
+++ Binaris/Binaris/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binaris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int db = 0;
+            Console.WriteLine("Adjon meg binerális szémot: ");
+            string szam = Console.ReadLine();
+            for (int i = 0; i < szam.Length; i++)
+            {
+                if (Convert.ToString(szam[i]) == "1") db++;
+                
+            }
+            Console.WriteLine("Megadott binerális szám {0} darab egyes szerepelt.", db);
+            Console.ReadLine();
+        }
+    }
+}
