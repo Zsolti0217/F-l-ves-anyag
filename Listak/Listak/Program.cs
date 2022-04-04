e78f2d10fff2ca05722fe401d8a10683a7a7a81e
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-04-04 19:01:38 +0200

    Listak

---
+++ Listak/Listak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Listak
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> lista = new List<int>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                lista.Add(rnd.Next(10, 100));
+            }
+            foreach(int i in lista)
+            {
+                Console.Write("{0}, ", i);
+            }
+
+        }
+    }
+}
