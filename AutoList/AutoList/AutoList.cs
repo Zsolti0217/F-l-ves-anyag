6c667f07ae71108e2395945d4e8bcd1e36207105
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-05-23 18:10:06 +0200

    AutoList

---
+++ AutoList/AutoList/AutoList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autolist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoLista auto = new AutoLista();
+            auto.Beolvas();
+            auto.kiir();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AutoList/AutoList/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoList
+{
+    internal class AutoLista
+    {
+        private class Auto
+        {
+            private string rendszam;
+            private string tipus;
+            private int gyartasiev;
+            private DateTime muszaki;
+            private string gyartmany;
+            private string tulajdonos;
+
+            public Auto() { }
+            public Auto(string rendszam, string tipus, int gyartasiev, DateTime muszaki, string gyartmany, string tulajdonos)
+            {
+                this.rendszam = rendszam;
+                this.tipus = tipus;
+                this.gyartasiev = gyartasiev;
+                this.muszaki = muszaki;
+                this.gyartmany = gyartmany;
+                this.tulajdonos = tulajdonos;
+            }
+            public void printAuto()
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", rendszam, gyartmany, tipus, gyartasiev, muszaki.ToShortDateString(), tulajdonos);
+            }
+
+            private List<Auto> autoLista;
+            private string fajlNev;
+            public AutoLista()
+            {
+                autoLista = new List<Auto>();
+            }
+            public void Beolvas()
+            {
+                string sor;
+                string[] mezok;
+                Console.WriteLine("Adja meg a szövegfájl nevét és elérési útvonalát!");
+                fajlNev = Console.ReadLine();
+                if (!fajlNev.EndsWith(".txt")) fajlNev += ".txt";
+                using (StreamReader sr = new StreamReader(fajlNev, Encoding.Default))
+                    while (sr.EndOfStream == false)
+                    {
+                        sor = sr.ReadLine();
+                        if (sor.Trim().Length == 0) continue;
+                        mezok = sor.Split(',');
+                        autoLista.Add(new Auto(mezok[0], mezok[1], mezok[2], DateTime.Parse(mezok[3]), DateTime.Parse(mezok[4]), mezok[5]));
+                    }
+            }
+            public void kiir()
+            {
+                foreach (Auto a in autoLista)
+                {
+                    a.printAuto();
+                }
+                Console.ReadKey();
+
+
+
+
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
