333d4e17c1f2b0875b67cb2ade7353222a451d68
Vadon Zsolt <90308512+Zsolti0217@users.noreply.github.com>
2022-06-01 10:19:59 +0200

    Képforgatás 2022.06.01

---
+++ Korbekorbe/korbekorbe/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace korbekorbe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Image = global::korbekorbe.Properties.Resources.Képkivágás;
+            this.label1.Location = new System.Drawing.Point(106, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 93);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Image = global::korbekorbe.Properties.Resources.Képkivágás1;
+            this.label2.Location = new System.Drawing.Point(106, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 93);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.Image = global::korbekorbe.Properties.Resources.Képkivágás3;
+            this.label4.Location = new System.Drawing.Point(406, 348);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 93);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.Image = global::korbekorbe.Properties.Resources.Képkivágás4;
+            this.label5.Location = new System.Drawing.Point(580, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 93);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "label1";
+            // 
+            // label6
+            // 
+            this.label6.Image = global::korbekorbe.Properties.Resources.Képkivágás5;
+            this.label6.Location = new System.Drawing.Point(580, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 93);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "label1";
+            // 
+            // label7
+            // 
+            this.label7.Image = global::korbekorbe.Properties.Resources.Képkivágás6;
+            this.label7.Location = new System.Drawing.Point(406, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 93);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "label1";
+            // 
+            // label8
+            // 
+            this.label8.Image = global::korbekorbe.Properties.Resources.Képkivágás7;
+            this.label8.Location = new System.Drawing.Point(270, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 93);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.Image = global::korbekorbe.Properties.Resources.Képkivágás2;
+            this.label3.Location = new System.Drawing.Point(270, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 93);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::korbekorbe.Properties.Resources.Képkivágás9;
+            this.button1.Location = new System.Drawing.Point(713, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 90);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::korbekorbe.Properties.Resources.Képkivágás8;
+            this.button2.Location = new System.Drawing.Point(12, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 81);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Korbekorbe/korbekorbe/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace korbekorbe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //balra
+            Image a = label1.Image;
+
+            label1.Image = label2.Image;
+            label2.Image = label3.Image;
+            label3.Image = label4.Image;
+            label4.Image = label5.Image;
+            label5.Image = label6.Image;
+            label6.Image = label7.Image;
+            label7.Image = label8.Image;
+            label8.Image = a;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   //jobbra
+            Image a = label1.Image;
+
+            label8.Image = label7.Image;
+            label7.Image = label6.Image;
+            label6.Image = label5.Image;
+            label5.Image = label5.Image;
+            label5.Image = label4.Image;
+            label4.Image = label3.Image;
+            label3.Image = label2.Image;
+            label2.Image = label1.Image;
+            label1.Image = a;
+        }
+    }
+}
